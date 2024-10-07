498ee4655bea6e6c2783dff05fa3a08f524e1cba
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2024-10-07 19:31:22 +0530

    contact functionalities fixes

--- website/otherScripts/contactFunctions.js
+++ website/otherScripts/contactFunctions.js
@@ -4,14 +4,14 @@ const callBtn = document.getElementById("call-btn");
 const phoneNumber = "+917440569391";
 const whatsappMessage = "Hello, I would like to get in touch!";
 const makePhoneCall = () => {
-  window.location.href = phoneNumber;
+  window.location.href = `tel:${phoneNumber}`;
 };
 
 const sendWhatsAppMessage = () => {
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
     whatsappMessage
   )}`;
-  window.open(whatsappURL, "_blank");
+  window.open(whatsappURL);
 };
 
 whatsappBtn.addEventListener("click", sendWhatsAppMessage);
